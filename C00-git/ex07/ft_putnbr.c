7671703bdf3595e05309f3043e609105e7892eb3
theshadow76 <vigopaul05@gmail.com>
2026-06-24 18:28:17 +0200

    message

---
+++ C00-git/ex07/ft_putnbr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   ft_putnbr.c                                         :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: viwalker <marvin@42.fr>                       +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2026/06/24 17:24:58 by viwalker       #+#    #+#                */
+/*   Updated: 2026/06/24 18:26:36 by viwalker       ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long int	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 0 && n <= 9)
+		ft_putchar(n + '0');
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
